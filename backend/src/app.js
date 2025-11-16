f4cb03d983f1e7322ebe5de26cc631fe607324d9
Abhi-batman <babluwayne@gmail.com>
2025-11-17 00:31:29 +0530

    app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,17 @@
 import express from "express";
 const app = express();
+import cookieParser from "cookie-parser";
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: "true",
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(cookieParser());
 
 export { app };
